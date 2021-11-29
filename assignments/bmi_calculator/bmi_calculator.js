7a1caa7f59221eb0bdf7fe64da8e70adb23df000
k mohammed althaf <89759298+Althafkm@users.noreply.github.com>
2021-11-29 20:26:57 +0530

    Update bmi_calculator.js
    done

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,8 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
+    mass=parseInt(mass);
+    height=parseInt(height)
     if (mass<=0 || height<=0)
     {
         return 'INVALID INPUT';
